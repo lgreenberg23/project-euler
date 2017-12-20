68fdaf6ed2dda4fd71ce4dfc7cb0f7034279295b
github username <github email address>
2017-12-20 12:24:46 -0500

    TDD

---
+++ testing/palindrome.js
@@ -0,0 +1,64 @@
+// iterate through all the possible combinations of 3-digit * 3-digit until
+// you hit a palindrome - ie nested for loop
+
+	//Array.from() makes a copy of an array
+
+function checkForPalindrome(number){
+	const forward = number.toString()
+	const reverse = forward.split('').reverse().join('')
+	return forward === reverse
+}
+
+
+
+//this object is what is returned by require in the spec file
+module.exports = {checkForPalindrome}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// def checkForPalindrome(int)
+// 	array = int.to_s.chars
+// 	half = (array.length/2)
+// 	looping_array = [*0..half]
+
+// 	looping_array.each do |num|
+// 		if !(array[num] == array[-1*(num+1)])
+// 			return false
+// 		end
+// 	end
+// 	return true
+// end 
+
+
+// def dumbSolution
+// 	product = 0
+// 	highest_product = 0
+// 	three_digit = [*100..999].reverse
+// 	second_array = [*100..999].reverse
+
+// 	three_digit.each do |num|
+// 		second_array.each do |second_num|
+// 			product = num*second_num
+// 			if checkForPalindrome(product)
+// 				if highest_product < product
+// 					highest_product = product
+// 				end
+// 			end
+// 		end
+// 	end
+// 	highest_product
+// end
---
+++ testing/palindrome.spec.js
@@ -0,0 +1,28 @@
+// check that 9009 returns true
+// what if it is a 1-digit number? is that a palindrome?
+// check that not a palindrome returns false
+// because if only check the cases where it returns true it could
+// return true every time
+
+//to run the test, type 'npm t' into the terminal (npm run test)
+// test command: jest
+
+const checkForPalindrome = require('./palindrome.js').checkForPalindrome 
+
+describe('checkForPalindrome', function(){
+	// context('if number is same forward and backward', function () {
+	// 	test('returns true')
+	})
+	test('returns true if number is the same forward and backward', function(){
+		//tell the test to do something and expect that that thing equals some value
+		//or asserts something about what we did
+		expect(checkForPalindrome(9009)).toEqual(true)
+	})
+	test('return true if it is a 1-digit number', function(){
+		expect(checkForPalindrome(1)).toEqual(true)
+	})
+	test('return false if number does not read the same forward & backward', function(){
+		expect(checkForPalindrome(2075)).toEqual(false)
+	})
+	//could write tests for other input like strings or negative numbers
+})
