f44cf108e6cd71bb658e7f76f8acfe3bd0830e53
github username <github email address>
2018-01-16 13:20:16 -0500

    maxSubarray 0(n2)

---
+++ testing/maxSubarray.js
@@ -0,0 +1,29 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ [-2,1,-3,4,-1,2,1,-5,4]
+ O(n2) would be for each number as the starting point, 
+ find the largest possible subarray, and then replace the maxSum with the largest one
+ keep track of a largest and a current sum at each index
+ */
+
+ //0(n2 solution)
+var maxSubArray = function(nums) {
+    let maxSum = 0
+    let currentSum = 0
+
+    let begin = nums[i]
+    
+    for(let i=0; i<nums.length-2; i++){
+    	begin = nums[i]
+    	currentSum = begin
+    	for (let j=i+1; j<nums.length-1; j++){
+    		currentSum += nums[j]
+    		if(currentSum > maxSum){
+    			maxSum = currentSum
+    		}
+    	}
+        
+    }
+    return maxSum
+};
--- testing/palindrome.js
+++ testing/palindrome.js
@@ -22,6 +22,15 @@ module.exports = {checkForPalindrome}
 
 
 
+function isPalindrome(string) {      
+   if (string.length === 0 || string.length === 1) {        
+       return true;    
+    }        
+   if (string[0] === string[string.length - 1]) {        
+     return isPalindrome( string.slice(1, string.length - 1) );     
+   }       
+  return false;
+};
 
 
 
